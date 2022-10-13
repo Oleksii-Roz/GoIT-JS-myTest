ac251e2afd9d3dcdb133ea2da8339ba6b176f11d
Oleksii Rozumii <o.rozumii@gmail.com>
2022-10-13 20:23:50 +0300

    do HW 22/48

--- index.js
+++ index.js
@@ -3217,3 +3217,155 @@
 // console.log("Все emails:", getUserEmails(users));
 
 // ЗАДАЧА 19/48------------------------------------------------------------------------
+// Вариант без функцией, результат присвоен результату переменным: evenNumbers и oddNumbers
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
+// const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+// console.log("evenNumbers:", evenNumbers);
+// console.log("oddNumbers:", oddNumbers);
+// console.log("numbers:", numbers);
+
+// Вариант с функцией, результат выводится вызовом функций: evenNumbers(numbers) и oddNumbers(numbers)
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// const evenNumbers = (numbers) => numbers.filter((number) => number % 2 === 0);
+// const oddNumbers = (numbers) => numbers.filter((number) => number % 2 !== 0);
+// console.log("evenNumbers:", evenNumbers(numbers));
+// console.log("oddNumbers:", oddNumbers(numbers));
+// console.log("numbers:", numbers);
+
+// ЗАДАЧА 20/48------------------------------------------------------------------------
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+
+// const allGenres = books.flatMap((book) => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (genre, index, array) => array.indexOf(genre) === index
+// );
+// console.log(allGenres);
+// console.log(uniqueGenres);
+
+// ЗАДАЧА 21/48------------------------------------------------------------------------
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+
+// // const topRatedBooks = books.filter((book) => book.rating >= MIN_RATING);
+// // const booksByAuthor = books.filter((book) => book.author === AUTHOR);
+
+// const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING); // стрелочная с деструктуризацией без функцией, результат присвоен результату переменной topRatedBooks
+// const booksByAuthor = books.filter(({ author }) => author === AUTHOR); // стрелочная с деструктуризацией без функцией, результат присвоен результату переменной booksByAuthor
+
+// console.log(topRatedBooks);
+// console.log(booksByAuthor);
+
+// ЗАДАЧА 22/48------------------------------------------------------------------------
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//   },
+// ];
+
+// const getUsersWithEyeColor = (users, color) =>
+//   users.filter(({ eyeColor }) => eyeColor === color);
+// console.log(getUsersWithEyeColor(users, "blue"));
+// console.log(getUsersWithEyeColor(users, "green"));
+// console.log(getUsersWithEyeColor(users, "brown"));
+
+// ЗАДАЧА 23/48------------------------------------------------------------------------
+// Change code below this line
+const getUsersWithAge = (users, minAge, maxAge) => {};
+// Change code above this line
