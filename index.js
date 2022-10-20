bbf0d2044ddb16be1a40bc6b77d3996f664cff93
Oleksii Rozumii <o.rozumii@gmail.com>
2022-10-20 17:08:24 +0300

    do HW 16/20

--- index.js
+++ index.js
@@ -5282,43 +5282,43 @@
 // padBoth(str) - получает парметр str (строку) и добавляет её в начало и в конец значения свойства value объекта который вызывает этот метод.
 // Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
 
-class StringBuilder {
-	constructor(initialValue) {
-		this.value = initialValue;
-	}
+// class StringBuilder {
+// 	constructor(initialValue) {
+// 		this.value = initialValue;
+// 	}
 
-	getValue() {
-		return this.value;
-	}
+// 	getValue() {
+// 		return this.value;
+// 	}
 
-	padStart(str) {
-		// return (this.value = str.concat(this.value));
-		// this.value = str + this.value;
-		this.value = str + this.value;
-	}
+// 	padStart(str) {
+// 		// return (this.value = str.concat(this.value));
+// 		// this.value = str + this.value;
+// 		this.value = str + this.value;
+// 	}
 
-	padEnd(str) {
-		// return (this.value = this.value.concat(str));
-		// this.value = this.value + str;
-		this.value += str;
-	}
+// 	padEnd(str) {
+// 		// return (this.value = this.value.concat(str));
+// 		// this.value = this.value + str;
+// 		this.value += str;
+// 	}
 
-	padBoth(str) {
-		// return (this.value = str.concat(this.value).concat(str));
-		// this.value = str + this.value + str;
-		this.padStart(str);
-		this.padEnd(str);
-	}
-}
+// 	padBoth(str) {
+// 		// return (this.value = str.concat(this.value).concat(str));
+// 		// this.value = str + this.value + str;
+// 		this.padStart(str);
+// 		this.padEnd(str);
+// 	}
+// }
 
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
 
 // ЗАДАЧА 12/20------------------------------------------------------------------------
 // // Выполни рефакторинг класса Car так, чтобы свойство brand было приватным и добавь два метода для публичного интерфейса, для чтения и изменения этого свойства.
@@ -5422,4 +5422,150 @@ console.log(builder.getValue()); // "=^.^="
 // builder.padBoth("=");
 // console.log(builder.getValue()); // "=^.^="
 // ЗАДАЧА 15/20------------------------------------------------------------------------
-// Выполни рефакторинг класса Car. Сделай свойства model и price приватными, также как #brand. Стандартизируй публичный интерфейс класса заменив уже объявленные методы на геттеры и сеттеры brand, model и price для взаимодействия с приватными свойствами.
+// // Выполни рефакторинг класса Car. Сделай свойства model и price приватными, также как #brand. Стандартизируй публичный интерфейс класса заменив уже объявленные методы на геттеры и сеттеры brand, model и price для взаимодействия с приватными свойствами.
+
+// // class Car {
+// // 	// Change code below this line
+// // 	#brand;
+
+// // 	constructor({ brand, model, price }) {
+// // 		this.#brand = brand;
+// // 		this.model = model;
+// // 		this.price = price;
+// // 	}
+
+// // 	getBrand() {
+// // 		return this.#brand;
+// // 	}
+
+// // 	changeBrand(newBrand) {
+// // 		this.#brand = newBrand;
+// // 	}
+
+// // 	getModel() {
+// // 		return this.model;
+// // 	}
+
+// // 	updateModel(newModel) {
+// // 		this.model = newModel;
+// // 	}
+
+// // 	getPrice() {
+// // 		return this.price;
+// // 	}
+
+// // 	setPrice(newPrice) {
+// // 		this.price = newPrice;
+// // 	}
+// // 	// Change code above this line
+// // }
+
+// // class Car {
+// // 	#brand;
+// // 	#model;
+// // 	#price;
+
+// // 	constructor({ brand, model, price }) {
+// // 		this.brand = brand;
+// // 		this.model = model;
+// // 		this.price = price;
+// // 	}
+
+// // 	get brand() {
+// // 		return this.brand;
+// // 	}
+
+// // 	set brand(newBrand) {
+// // 		this.brand = newBrand;
+// // 	}
+
+// // 	get model() {
+// // 		return this.model;
+// // 	}
+
+// // 	set model(newModel) {
+// // 		this.model = newModel;
+// // 	}
+
+// // 	get price() {
+// // 		return this.price;
+// // 	}
+
+// // 	set price(newPrice) {
+// // 		this.price = newPrice;
+// // 	}
+// // }
+
+// class Car {
+// 	#brand; // # - объявление приватных свойств
+// 	#model; // # - объявление приватных свойств
+// 	#price; // # - объявление приватных свойств
+
+// 	constructor({ brand, model, price }) {
+// 		this.#brand = brand;
+// 		this.#model = model;
+// 		this.#price = price;
+// 	}
+
+// 	get brand() {
+// 		return this.#brand;
+// 	}
+
+// 	set brand(newBrand) {
+// 		this.#brand = newBrand;
+// 	}
+
+// 	get model() {
+// 		return this.#model;
+// 	}
+
+// 	set model(newModel) {
+// 		this.#model = newModel;
+// 	}
+
+// 	get price() {
+// 		return this.#price;
+// 	}
+
+// 	set price(newPrice) {
+// 		this.#price = newPrice;
+// 	}
+// }
+
+// ЗАДАЧА 16/20------------------------------------------------------------------------
+// // Выполни рефакторинг класса Car. Добавь публичное статическое свойство MAX_PRICE со значением 50000 - максимально допустимая цена автомобиля.
+
+// // Добавь сеттеру price проверку передаваемого значения параметра newPrice. Если оно больше чем MAX_PRICE, сеттер ничего не делает, а если меньше или равно, то перезаписывает цену автомобиля.
+// class Car {
+// 	// Change code above this line
+// 	static MAX_PRICE = 50000; // - публичное статическое свойство MAX_PRICE со значением 50000
+
+// 	#price;
+
+// 	constructor({ price }) {
+// 		this.#price = price;
+// 	}
+
+// 	get price() {
+// 		return this.#price;
+// 	}
+
+// 	set price(newPrice) {
+// 		if (newPrice <= Car.MAX_PRICE) {
+// 			this.#price = newPrice;
+// 		} // Car.MAX_PRICE - обращение к статическому свойству MAX_PRICE
+// 	}
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// console.log(Car.MAX_PRICE); // 50000
+
+// ЗАДАЧА 17/20------------------------------------------------------------------------
