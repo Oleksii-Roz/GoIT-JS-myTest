384066738b58635002e113f6952c6efd0349bf4a
Oleksii Rozumii <o.rozumii@gmail.com>
2022-10-20 10:17:28 +0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// "use strict";
+// "use strict"; // - это установка, которая заставляет код обрабатываться в строгом режиме.
 
 //
 //
@@ -5282,39 +5282,43 @@
 // padBoth(str) - получает парметр str (строку) и добавляет её в начало и в конец значения свойства value объекта который вызывает этот метод.
 // Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
 
-// class StringBuilder {
-// 	constructor(initialValue) {
-// 		this.value = initialValue;
-// 	}
+class StringBuilder {
+	constructor(initialValue) {
+		this.value = initialValue;
+	}
 
-// 	getValue() {
-// 		return this.value;
-// 	}
+	getValue() {
+		return this.value;
+	}
 
-// 	padStart(value) {
-// 		// return (this.value = value.concat(this.value));
-// 		this.value = value + this.value;
-// 	}
+	padStart(str) {
+		// return (this.value = str.concat(this.value));
+		// this.value = str + this.value;
+		this.value = str + this.value;
+	}
 
-// 	padEnd(value) {
-// 		// return (this.value = this.value.concat(value));
-// 		this.value = this.value + value;
-// 	}
+	padEnd(str) {
+		// return (this.value = this.value.concat(str));
+		// this.value = this.value + str;
+		this.value += str;
+	}
 
-// 	padBoth(value) {
-// 		// return (this.value = value.concat(this.value).concat(value));
-// 		this.value = value + this.value + value;
-// 	}
-// }
+	padBoth(str) {
+		// return (this.value = str.concat(this.value).concat(str));
+		// this.value = str + this.value + str;
+		this.padStart(str);
+		this.padEnd(str);
+	}
+}
 
-// const builder = new StringBuilder(".");
-// console.log(builder.getValue()); // "."
-// builder.padStart("^");
-// console.log(builder.getValue()); // "^."
-// builder.padEnd("^");
-// console.log(builder.getValue()); // "^.^"
-// builder.padBoth("=");
-// console.log(builder.getValue()); // "=^.^="
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
 
 // ЗАДАЧА 12/20------------------------------------------------------------------------
 // // Выполни рефакторинг класса Car так, чтобы свойство brand было приватным и добавь два метода для публичного интерфейса, для чтения и изменения этого свойства.
@@ -5383,39 +5387,39 @@
 // // // Выполни рефакторинг класса StringBuilder, сделав свойство value приватным.
 
 // // // Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
-class StringBuilder {
-	// Change code below this line
-	#value;
-	constructor(initialValue) {
-		this.#value = initialValue;
-	}
+// class StringBuilder {
+// 	// Change code below this line
+// 	#value;
+// 	constructor(initialValue) {
+// 		this.#value = initialValue;
+// 	}
 
-	getValue() {
-		return this.#value;
-	}
+// 	getValue() {
+// 		return this.#value;
+// 	}
 
-	padEnd(str) {
-		this.#value += str;
-	}
+// 	padEnd(str) {
+// 		this.#value += str;
+// 	}
 
-	padStart(str) {
-		this.#value = str + this.#value;
-	}
+// 	padStart(str) {
+// 		this.#value = str + this.#value;
+// 	}
 
-	padBoth(str) {
-		this.padStart(str);
-		this.padEnd(str);
-	}
-}
+// 	padBoth(str) {
+// 		this.padStart(str);
+// 		this.padEnd(str);
+// 	}
+// }
 
-// Change code above this line
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
 // ЗАДАЧА 15/20------------------------------------------------------------------------
 // Выполни рефакторинг класса Car. Сделай свойства model и price приватными, также как #brand. Стандартизируй публичный интерфейс класса заменив уже объявленные методы на геттеры и сеттеры brand, model и price для взаимодействия с приватными свойствами.
