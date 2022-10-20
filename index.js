769e8b18a386b28aee5698df1a51b59fc5c4c001
Oleksii Rozumii <o.rozumii@gmail.com>
2022-10-20 22:46:09 +0300

    do HW  17/20

--- index.js
+++ index.js
@@ -5569,3 +5569,52 @@
 // console.log(Car.MAX_PRICE); // 50000
 
 // ЗАДАЧА 17/20------------------------------------------------------------------------
+// // Добавь классу Car публичный статический метод checkPrice(price), принимающий цену автомобиля. Метод должен сравнить значения параметра price и приватного статического свойства MAX_PRICE.
+
+// // Если цена автомобиля превышает максимальную, метод должен вернуть строку "Error! Price exceeds the maximum".
+// // В противном случае метод должен вернуть строку "Success! Price is within acceptable limits".
+// // Под объявлением класса мы добавили инициализацию экземпляра и вызовы методов, чтобы показать как будет использоваться метод checkPrice(price).
+// // class Car {
+// //   static #MAX_PRICE = 50000;
+// //   // Change code below this line
+
+// //   // Change code above this line
+// //   constructor({ price }) {
+// //     this.price = price;
+// //   }
+// // }
+
+// // const audi = new Car({ price: 36000 });
+// // const bmw = new Car({ price: 64000 });
+
+// // console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// // console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// class Car {
+// 	static #MAX_PRICE = 50000;
+
+// 	static checkPrice(price) {
+// 		if (price >= Car.#MAX_PRICE) {
+// 			this.price = price;
+// 			return "Error! Price exceeds the maximum";
+// 		}
+// 		return "Success! Price is within acceptable limits";
+// 	}
+
+// 	constructor({ price }) {
+// 		this.price = price;
+// 	}
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// console.log(Car.checkPrice(36000));
+// console.log(Car.checkPrice(18000));
+// console.log(Car.checkPrice(64000));
+// console.log(Car.checkPrice(57000));
+
+// ЗАДАЧА 18/20------------------------------------------------------------------------
