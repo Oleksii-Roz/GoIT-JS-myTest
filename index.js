1b1afcd2e647b80241477773ec91789c2ebf8300
Oleksii Rozumii <o.rozumii@gmail.com>
2022-10-22 22:14:32 +0300

    do HW 20/20

--- index.js
+++ index.js
@@ -5662,3 +5662,182 @@
 // console.log(Admin.AccessLevel.SUPERUSER);
 
 // ЗАДАЧА 19/20------------------------------------------------------------------------
+// // Добавь классу Admin метод constructor, который принимает один параметр - объект настроек с двумя свойствами email и accessLevel. Добавь классу Admin публичное свойство accessLevel, значение которого будет передаваться при вызове конструктора.
+
+// // Чтобы показать как будет использоваться класс Admin мы добавили инициализацию экземпляра под объявлением класса.
+// // class User {
+// //   email;
+
+// //   constructor(email) {
+// //     this.email = email;
+// //   }
+
+// //   get email() {
+// //     return this.email;
+// //   }
+
+// //   set email(newEmail) {
+// //     this.email = newEmail;
+// //   }
+// // }
+
+// // class Admin extends User {
+// //   // Change code below this line
+
+// //   static AccessLevel = {
+// //     BASIC: "basic",
+// //     SUPERUSER: "superuser",
+// //   };
+
+// //   // Change code above this line
+// // }
+
+// // const mango = new Admin({
+// //   email: "mango@mail.com",
+// //   accessLevel: Admin.AccessLevel.SUPERUSER,
+// // });
+
+// // console.log(mango.email); // "mango@mail.com"
+// // console.log(mango.accessLevel); // "superuser"
+
+// class User {
+// 	email;
+
+// 	constructor(email) {
+// 		this.email = email;
+// 	}
+
+// 	get email() {
+// 		return this.email;
+// 	}
+
+// 	set email(newEmail) {
+// 		this.email = newEmail;
+// 	}
+// }
+
+// class Admin extends User {
+// 	// Change code below this line
+
+// 	static AccessLevel = {
+// 		BASIC: "basic",
+// 		SUPERUSER: "superuser",
+// 	};
+
+// 	constructor({ email, accessLevel }) {
+// 		super(email);
+// 		this.accessLevel = accessLevel;
+// 	}
+// 	// Change code above this line
+// }
+
+// const mango = new Admin({
+// 	email: "mango@mail.com",
+// 	accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// ЗАДАЧА 20/20------------------------------------------------------------------------
+// Добавь классу Admin следующие свойства и методы.
+
+// Публичное свойство blacklistedEmails для хранения чёрного списка почтовых адресов пользователей. Значение по умолчанию это пустой массив.
+// Публичный метод blacklist(email) для добавления почты в чёрный список. Метод должен добавлять значение параметра email в массив хранящийся в свойстве blacklistedEmails.
+// Публичный метод isBlacklisted(email) для проверки почты в чёрном списке. Метод должен проверять наличие значения параметра email в массиве хранящемся в свойстве blacklistedEmails и возвращать true или false.
+// После объявления класса мы добавили инициализацию экземпляра и вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//   }
+
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+class User {
+	email;
+
+	constructor(email) {
+		this.email = email;
+	}
+
+	get email() {
+		return this.email;
+	}
+
+	set email(newEmail) {
+		this.email = newEmail;
+	}
+}
+class Admin extends User {
+	// Change code below this line
+
+	static AccessLevel = {
+		BASIC: "basic",
+		SUPERUSER: "superuser",
+	};
+
+	constructor({ email, accessLevel, blacklistedEmails = [] }) {
+		super(email);
+		this.accessLevel = accessLevel;
+		this.blacklistedEmails = blacklistedEmails;
+	}
+
+	blacklist(email) {
+		return this.blacklistedEmails.push(email);
+	}
+
+	isBlacklisted(email) {
+		return this.blacklistedEmails.includes(email);
+	}
+	// Change code above this line
+}
+
+const mango = new Admin({
+	email: "mango@mail.com",
+	accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
