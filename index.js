3a4310a3a12827901ddaf15563c4897efe76b4c7
Oleksii Rozumii <o.rozumii@gmail.com>
2022-11-03 11:31:34 +0200

    &&&

--- index.js
+++ index.js
@@ -5790,54 +5790,87 @@
 // console.log(mango.isBlacklisted("mango@mail.com")); // false
 // console.log(mango.isBlacklisted("poly@mail.com")); // true
 
-class User {
-	email;
+// class User {
+// 	email;
 
-	constructor(email) {
-		this.email = email;
-	}
+// 	constructor(email) {
+// 		this.email = email;
+// 	}
 
-	get email() {
-		return this.email;
-	}
+// 	get email() {
+// 		return this.email;
+// 	}
 
-	set email(newEmail) {
-		this.email = newEmail;
-	}
-}
-class Admin extends User {
-	// Change code below this line
+// 	set email(newEmail) {
+// 		this.email = newEmail;
+// 	}
+// }
+// class Admin extends User {
+// 	// Change code below this line
 
-	static AccessLevel = {
-		BASIC: "basic",
-		SUPERUSER: "superuser",
-	};
+// 	static AccessLevel = {
+// 		BASIC: "basic",
+// 		SUPERUSER: "superuser",
+// 	};
 
-	constructor({ email, accessLevel, blacklistedEmails = [] }) {
-		super(email);
-		this.accessLevel = accessLevel;
-		this.blacklistedEmails = blacklistedEmails;
-	}
+// 	constructor({ email, accessLevel, blacklistedEmails = [] }) {
+// 		super(email);
+// 		this.accessLevel = accessLevel;
+// 		this.blacklistedEmails = blacklistedEmails;
+// 	}
 
-	blacklist(email) {
-		return this.blacklistedEmails.push(email);
-	}
+// 	blacklist(email) {
+// 		return this.blacklistedEmails.push(email);
+// 	}
 
-	isBlacklisted(email) {
-		return this.blacklistedEmails.includes(email);
-	}
-	// Change code above this line
-}
+// 	isBlacklisted(email) {
+// 		return this.blacklistedEmails.includes(email);
+// 	}
+// 	// Change code above this line
+// }
 
-const mango = new Admin({
-	email: "mango@mail.com",
-	accessLevel: Admin.AccessLevel.SUPERUSER,
-});
+// const mango = new Admin({
+// 	email: "mango@mail.com",
+// 	accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
 
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.accessLevel); // "superuser"
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
 
-mango.blacklist("poly@mail.com");
-console.log(mango.blacklistedEmails); // ["poly@mail.com"]
-console.log(mango.isBlacklisted("mango@mail.com")); // false
-console.log(mango.isBlacklisted("poly@mail.com")); // true
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+// JS HW6------------------------------------------------------------------------
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = body.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
+//---
+const listWithId = document.querySelector("#menu");
+listWithId.style.textTransform = "uppercase";
+listWithId.style.fontSize = "24px";
+console.log(listWithId);
+
+const listWithClass = document.querySelector(".menu");
+console.log(listWithClass);
+
+const menuItemsByTagName = document.querySelectorAll("li");
+console.log(menuItemsByTagName);
+
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
+
+const firstMenuItem = document.querySelector(".menu-item");
+firstMenuItem.style.color = "tomato";
+console.log(firstMenuItem);
